3fc794b44579ee61d974264de8c291b8a125160e
Kalvinz <vovas0922@gmail.com>
2020-10-28 17:34:08 +0200

    Implement interfaces for services. Closes #5.

---
+++ pet-clinic-data/src/main/java/kvinz/springframework/petclinic/services/VetService.java
@@ -0,0 +1,13 @@
+package kvinz.springframework.petclinic.services;
+
+import kvinz.springframework.petclinic.model.Vet;
+
+import java.util.Set;
+
+public interface VetService {
+    Vet findById(Long id);
+
+    Vet save(Vet vet);
+
+    Set<Vet> findAll();
+}
