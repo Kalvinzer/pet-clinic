588792483d28cf5872461eddead5a7b00526b1ec
Kalvinz <vovas0922@gmail.com>
2020-11-01 14:38:21 +0200

    Added auto id generation. Closes #23.

--- pet-clinic-web/src/main/java/kvinz/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kvinz/springframework/petclinic/bootstrap/DataLoader.java
@@ -22,14 +22,12 @@ public class DataLoader implements CommandLineRunner {
     public void run(String... args) throws Exception {
 
         Owner owner1 = new Owner();
-        owner1.setId(1L);
         owner1.setFirstName("Ivan");
         owner1.setLastName("Korovin");
 
         ownerService.save(owner1);
 
         Owner owner2 = new Owner();
-        owner2.setId(2L);
         owner2.setFirstName("Marianna");
         owner2.setLastName("Kiss");
 
@@ -38,14 +36,12 @@ public class DataLoader implements CommandLineRunner {
         System.out.println("-------Loaded owners");
 
         Vet vet1 = new Vet();
-        vet1.setId(1L);
         vet1.setFirstName("Maksym");
         vet1.setLastName("Maksymov");
 
         vetService.save(vet1);
 
         Vet vet2 = new Vet();
-        vet2.setId(2L);
         vet2.setFirstName("Alex");
         vet2.setLastName("Panin");
 
