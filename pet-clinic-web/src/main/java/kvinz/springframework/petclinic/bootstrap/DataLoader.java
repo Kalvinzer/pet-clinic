4ed0dc1698db8d68a8e44d080de38ead841e14dd
Kalvinz <vovas0922@gmail.com>
2020-11-04 16:42:55 +0200

    added pettype bootstrap data  #32

--- pet-clinic-web/src/main/java/kvinz/springframework/petclinic/bootstrap/DataLoader.java
+++ pet-clinic-web/src/main/java/kvinz/springframework/petclinic/bootstrap/DataLoader.java
@@ -1,8 +1,10 @@
 package kvinz.springframework.petclinic.bootstrap;
 
 import kvinz.springframework.petclinic.model.Owner;
+import kvinz.springframework.petclinic.model.PetType;
 import kvinz.springframework.petclinic.model.Vet;
 import kvinz.springframework.petclinic.services.OwnerService;
+import kvinz.springframework.petclinic.services.PetTypeService;
 import kvinz.springframework.petclinic.services.VetService;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
@@ -12,14 +14,25 @@ public class DataLoader implements CommandLineRunner {
 
     private final OwnerService ownerService;
     private final VetService  vetService;
+    private final PetTypeService petTypeService;
 
-    public DataLoader(OwnerService ownerService, VetService vetService) {
+    public DataLoader(OwnerService ownerService, VetService vetService, PetTypeService petTypeService) {
         this.ownerService = ownerService;
         this.vetService = vetService;
+        this.petTypeService = petTypeService;
     }
+
 
     @Override
     public void run(String... args) throws Exception {
+
+        PetType dog = new PetType();
+        dog.setName("dog");
+        PetType saveDogPetType = petTypeService.save(dog);
+
+        PetType cat = new PetType();
+        dog.setName("cat");
+        PetType saveCatPetType = petTypeService.save(cat);
 
         Owner owner1 = new Owner();
         owner1.setFirstName("Ivan");
